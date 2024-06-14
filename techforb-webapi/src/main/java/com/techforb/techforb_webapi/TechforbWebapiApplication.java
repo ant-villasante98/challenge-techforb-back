b991232b9551e2b674fa86eb1516926b178a325f
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-06-14 18:20:13 -0300

    primer commit

---
+++ techforb-webapi/src/main/java/com/techforb/techforb_webapi/TechforbWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.techforb.techforb_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechforbWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechforbWebapiApplication.class, args);
+	}
+
+}
---
+++ techforb-webapi/src/test/java/com/techforb/techforb_webapi/TechforbWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techforb.techforb_webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechforbWebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
