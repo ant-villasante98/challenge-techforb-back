18b160ea95b927508b55e2aae48e04bedddfdee8
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-06-14 22:19:10 -0300

    estructura inicial

---
+++ techforb-webapi/src/main/java/com/techforb/techforb_webapi/core/models/Plant.java
@@ -0,0 +1,9 @@
+package com.techforb.techforb_webapi.core.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "plants")
+public class Plant {
+}
