86b2a09dd0c89cf521d09f400cbc27e45d9d8bfa
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-06-20 20:19:02 -0300

    prueba unitaria de servicio de plantas

--- techforb-webapi/src/test/java/com/techforb/techforb_webapi/core/services/Implement/PlantServiceImplTest.java
+++ techforb-webapi/src/test/java/com/techforb/techforb_webapi/core/services/Implement/PlantServiceImplTest.java
@@ -1,10 +1,57 @@
 package com.techforb.techforb_webapi.core.services.Implement;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.techforb.techforb_webapi.core.models.Country;
+import com.techforb.techforb_webapi.core.models.Plant;
+import com.techforb.techforb_webapi.core.reposittories.PlantRepository;
+
+import io.jsonwebtoken.lang.Assert;
+
+@ExtendWith(MockitoExtension.class)
 public class PlantServiceImplTest {
+    @Mock
+    private PlantRepository plantRepository;
+
+    @InjectMocks
+    private PlantServiceImpl plantService;
+
     @Test
     void testCreate() {
+        // Arrage
+        String name = "planta 1";
+        String countryName = "Argentina";
+        String countryFlag = "url de bandera";
+        Plant expectedPlant = new Plant(0, name, 0, 0, 0, 0, new Country(countryName, countryFlag));
+
+        when(plantRepository.saveAndFlush(any(Plant.class))).thenReturn(any(Plant.class));
+
+        // Act
+        plantService.create(name, countryName, countryFlag);
+
+        ArgumentCaptor<Plant> plantCaptor = ArgumentCaptor.forClass(Plant.class);
+        verify(plantRepository, times(1)).saveAndFlush(plantCaptor.capture());
+
+        // Assert
+        Plant capturePlant = plantCaptor.getValue();
+        assertEquals(expectedPlant.getName(), capturePlant.getName());
+        assertEquals(expectedPlant.getNumberOfDisabledSensors(), capturePlant.getNumberOfDisabledSensors());
+        assertEquals(expectedPlant.getNumberOfMediumAlerts(), capturePlant.getNumberOfMediumAlerts());
+        assertEquals(expectedPlant.getNumberOfReadings(), capturePlant.getNumberOfReadings());
+        assertEquals(expectedPlant.getNumberOfRedAlerts(), capturePlant.getNumberOfRedAlerts());
+        assertEquals(expectedPlant.getCountry().getName(), capturePlant.getCountry().getName());
+        assertEquals(expectedPlant.getCountry().getFlagImage(), capturePlant.getCountry().getFlagImage());
 
     }
 
